38992e912e5682ed3dac9630e3eb05fc9ebb1896
Alfredo Rodriguez <arodrigu12@hotmail.com>
2019-09-28 14:18:17 -0400

    Completed List and Create Blog Posts

--- main.py
+++ main.py
@@ -19,20 +19,43 @@ class Blog(db.Model):
         self.title = title
         self.body = body
 
-@app.route('/', methods=['POST', 'GET'])
-def index():
+@app.route('/newpost', methods=['POST', 'GET'])
+def newpost():
+
+    no_title_error = ''
+    no_body_error = ''
+    error_condition = False
+    title = ''
+    body = ''
 
     if request.method == 'POST': # POST for processing, GET for rendering
-        title = request.form['title']
-        body = request.form['body']   
-        new_blog = Blog(title, body)
-        db.session.add(new_blog)
-        db.session.commit()
+        title = request.form['title'].strip()
+        body = request.form['body'].strip()
 
+        if not title:
+            no_title_error = 'Please enter a blog title'
+            error_condition = True
+
+        if not body:
+            no_body_error = 'Please fill in the blog body'
+            error_condition = True
+
+        if not error_condition:
+            new_blog = Blog(title, body)
+            db.session.add(new_blog)
+            db.session.commit()
+            return redirect('/blog')
+
+    return render_template('newpost.html', title="Build-a-Blog",
+        no_title_error=no_title_error, no_body_error=no_body_error, body=body, blog_title=title)
+
+@app.route('/blog')
+def blog():
     blog_entries = Blog.query.all()
 
-    return render_template('index.html', title="Build-a-Blog", 
+    return render_template('blog.html', title="Build-a-Blog", 
         blog_entries=blog_entries)
+
 
 if __name__ == '__main__':
     app.run()
